9b9408d77062d7bfbf9aef337ce42e86819eb061
byEduarda <albuquerque.zxl@gmail.com>
2025-08-01 01:50:57 -0300

    Update tarefasRoutes.js

--- src/routes/tarefasRoutes.js
+++ src/routes/tarefasRoutes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 let tarefas = [
     {
         id: 1,
-        título: "Estudar Node.js",
-        descrição: "Ver aula PretaLab e revisar código",
+        titulo: "Estudar Node.js",
+        descricao: "Ver aula PretaLab e revisar código",
         status: "pendente",
         dataLimite: "04-08-2025"
     }
@@ -14,5 +14,26 @@ let tarefas = [
 router.get("/", (req, res) => {
     res.status(200).json(tarefas);
 });
+
+routes.post("/", (req, res) => {
+    const { titulo, descricao, status, dataLimite } = req.body;
+
+    if(!titulo || !descricao) {
+        return res.status(400).json({mensagem: "Título e descrição são obrigatórios."});
+    }
+
+    const novaTarefa = {
+        id: tarefas.length + 1,
+        titulo, 
+        descricao,
+        status: status || "pendente",
+        dataLimite: dataLimite || null
+    };
+
+    tarefa.push(novaTarefa);
+
+    res.status(200).json(novaTarefa);
+});
+
 
 module.exports = router;
