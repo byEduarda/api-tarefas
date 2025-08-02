fe39607c1053bc055e993966070028bbd55c78a5
byEduarda <albuquerque.zxl@gmail.com>
2025-08-01 21:22:01 -0300

    Update tarefasRoutes.js

--- src/routes/tarefasRoutes.js
+++ src/routes/tarefasRoutes.js
@@ -1,38 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const Tarefa = require("../models/tarefa")
 
-let tarefas = [
-    {
-        id: 1,
-        titulo: "Estudar Node.js",
-        descricao: "Ver aula PretaLab e revisar código",
-        status: "pendente",
-        dataLimite: "04-08-2025"
-    }
-];
-
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    const tarefas = await Tarefa.find();
     res.status(200).json(tarefas);
 });
 
-routes.post("/", (req, res) => {
-    const { titulo, descricao, status, dataLimite } = req.body;
 
-    if(!titulo || !descricao) {
-        return res.status(400).json({mensagem: "Título e descrição são obrigatórios."});
+router.post("/", async (req, res) => {
+    try {
+        const { titulo, descricao, status, dataLimite } = req.body;
+
+        if(!titulo || !descricao) {
+            return res.status(400).json({mensagem: "Título e descrição são obrigatórios."});
+        }
+
+        const novaTarefa = new Tarefa({ titulo, descricao, status, dataLimite });
+        const tarefaSalva = await novaTarefa.save();
+
+
+        res.status(201).json(tarefaSalva);
+    }catch (error) {
+        res.status(500).json({ erro: erro.message });
     }
-
-    const novaTarefa = {
-        id: tarefas.length + 1,
-        titulo, 
-        descricao,
-        status: status || "pendente",
-        dataLimite: dataLimite || null
-    };
-
-    tarefa.push(novaTarefa);
-
-    res.status(200).json(novaTarefa);
 });
 
 
