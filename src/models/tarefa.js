4dffef93d155935875fbafab3d4ced9ba0545e2b
byEduarda <albuquerque.zxl@gmail.com>
2025-08-01 21:21:58 -0300

    Create tarefa.js

---
+++ src/models/tarefa.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const tarefaSchema = new mongoose.Schema ({
+    titulo: { type: String, required: true },
+    descricao: { type: String, required: true },
+    status: { type: String, default: "pendente" },
+    dataLimite: { type: Date },
+});
+
+const Tarefa = mongoose.model("Tarefa", tarefaSchema);
+
+module.exports = Tarefa;
