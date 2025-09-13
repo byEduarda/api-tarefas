f2e04ff7e2812da40e4199b292de1e90ecc94a0d
byEduarda <albuquerque.zxl@gmail.com>
2025-09-13 11:50:38 -0300

    refatoração

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import app from './app.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoUri = process.env.MONGO_URL; 
+const PORT = process.env.PORT || 3000;
+
+if (!mongoUri) {
+  console.error("Erro: A variável de ambiente MONGO_URI não está definida.");
+  process.exit(1);
+}
+
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log('Conectado ao MongoDB Atlas com sucesso!');
+    
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Erro de conexão com o MongoDB:', err);
+  });
